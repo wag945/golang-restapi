c72f7fe9ad244ce6bd5528ced37a280ca630154a
Bill Ganley <wag945@gmail.com>
2021-07-27 11:18:55 -0400

    Add tracks to the list?

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 type Track struct {
 	TrackNum string `json:"TrackNumber"`
+	Category string `json:"Category"`
 	Id string `json:"Id"`
 	Lat string `json:"Lat"`
 	Lon string `json:"Lon"`
@@ -28,13 +29,15 @@ func handleRequests() {
 }
 
 func getTracks(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: getTracks")
+	fmt.Println("Endpoint Hit: tracks")
 	json.NewEncoder(w).Encode(Tracks)
 }
 
 func main() {
 	Tracks = []Track{
-		Track{TrackNum: "1", Id: "Friend", Lat: "37.0", Lon: "-74.0"},
+		Track{TrackNum: "1", Category: "Surface", Id: "Friend", Lat: "37.0", Lon: "-74.0"},
+		Track{TrackNum: "2", Category: "Air", Id: "Unknown", Lat: "37.5", Lon: "-74.3"},
+		Track{TrackNum: "3", Category: "Air", Id: "Unknown", Lat: "36.2", Lon: "-74.9"},
 	}
 	handleRequests()
 }
